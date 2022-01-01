72b7be062bbec43f9e1c37ef60152984a5963c53
Fahad98723 <md.ravi63@gmail.com>
2022-01-01 23:47:47 +0600

    'dr'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        <h1>Log In </h1>
+      </div>
+      <form action="">
+        <input type="email" name='email' className='' placeholder='You Email' />
+        <input type="password" name='password' className='' placeholder='Your Password' />
+      </form>
     </div>
   );
 }
