b6e20350a06480fafee1dc93b1079917a8e5903d
Fahad98723 <md.ravi63@gmail.com>
2022-01-02 02:22:48 +0600

    'done'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import Home from './Pages/homePage/home/Home';
-function App() {
-  return (
-    <div className="App">
-
-      {/* ===================Home Page (মোঃ রাহাত ইসলাম ) ===============*/}
-
-      <Home />
-
-      {/* ========================== */}
-
-      <div className="heading">
-        <h1>Log In </h1>
-      </div>
-      <form action="">
-        <input type="email" name='email' className='' placeholder='You Email' />
-        <input type="password" name='password' className='' placeholder='Your Password' />
-      </form>
-=======
 import LogIn from './Pages/Form/LogIn/LogIn';
 import {
   BrowserRouter,
@@ -28,16 +8,18 @@ import {
   Link
 } from "react-router-dom";
 import Register from './Pages/Form/Register/Register';
+import Home from './Pages/homePage/Home/Home';
 function App() {
   return (
     <div className="">
       <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/register" element={<Register />} />
       </Routes>
       </BrowserRouter>
->>>>>>> fbc46c433255f16619cbbc527c51e48a4f7a8182
     </div>
   );
 }
