0eb560bce38e483d42362a9143687aa424948cff
Fahad98723 <md.ravi63@gmail.com>
2022-01-02 23:25:09 +0600

    'done'

--- src/App.js
+++ src/App.js
@@ -7,13 +7,9 @@ import {
   Route,
 } from "react-router-dom";
 import Register from './Pages/Form/Register/Register';
-<<<<<<< HEAD
-import Home from './Pages/homePage/home/Home';
-=======
 import Home from './Pages/Home/Home/Home'
 import Navigation from './Pages/Shared/Navigation/Navigation';
 import Footer from './Pages/Home/Footer/Footer';
->>>>>>> 1a1cdbe5ee0754765cd24f44b1255f85e2cf1cc4
 
 function App() {
   return (
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,9 +3,11 @@ import Navigation from '../../Shared/Navigation/Navigation';
 import Footer from '../Footer/Footer'
 import ContactForm from '../../Form/ContactForm/ContactForm'
 import ChooseUs from '../ChooseUs/ChooseUs';
+import Slider from '../Slider/Slider';
 const Home = () => {
     return (
         <>
+        <Slider>ss</Slider>
         <ChooseUs></ChooseUs>
         <ContactForm></ContactForm>
         </>        
