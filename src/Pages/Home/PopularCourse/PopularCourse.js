d7a8f60a6fc3317129e0cc7d71280a0a6a27f9bb
robinhood6996 <robinypb@yahoo.com>
2022-01-03 14:44:55 +0600

    course section done

--- src/Pages/Home/PopularCourse/PopularCourse.js
+++ src/Pages/Home/PopularCourse/PopularCourse.js
@@ -22,94 +22,120 @@ SwiperCore.use([FreeMode, Pagination]);
 
 const PopularCourse = () => {
     return (
-        <div className='container mb-10'>
-            <h2>Our Popular Courses</h2>
-            <p>It is a long established fact that a reader will be distracted by<br /> the readable content of a page</p>
+        <div className='popular-course-section pt-5 pb-5'>
+            <div className='container mb-4 popular-course-section'>
+                <h2 className="fw-bold">Our Popular Courses</h2>
+                <p style={{ fontSize: '20px' }}>It is a long established fact that a reader will be distracted by<br /> the readable content of a page</p>
 
-            <div>
-                <Swiper slidesPerView={3} spaceBetween={30} freeMode={true} pagination={{
-                    "clickable": true
-                }} className="mySwiper">
-                    <Row xs={1} md={2} className="g-4">
-                        <SwiperSlide>
-                            <Col>
-                                <Card>
-                                    <Card.Img variant="top" src={php} />
-                                    <Card.Body>
-                                        <p className="fw-bold" style={{ color: '#A3A2A4' }}>Programming</p>
-                                        <Card.Title style={{ fontWeight: 'bold' }}>Learn PHP Programming From Scratch</Card.Title>
-                                        <Card.Text>
-                                            This is a longer card with supporting text below as a natural
-                                            lead-in to additional content. This content is a little bit longer.
-                                        </Card.Text>
-                                        <div className="price-review d-flex justify-content-between align-items-center">
-                                            <div className="price"><p className="fw-bold fs-4">$120</p></div>
-                                            <div className="buy-btn"><Button variation="success" className="buy-btn">Enroll Now</Button></div>
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <Col>
-                                <Card>
-                                    <Card.Img variant="top" src={php} />
-                                    <Card.Body>
-                                        <p className="fw-bold" style={{ color: '#A3A2A4' }}>Programming</p>
-                                        <Card.Title style={{ fontWeight: 'bold' }}>Learn PHP Programming From Scratch</Card.Title>
-                                        <Card.Text>
-                                            This is a longer card with supporting text below as a natural
-                                            lead-in to additional content. This content is a little bit longer.
-                                        </Card.Text>
-                                        <div className="price-review d-flex justify-content-between align-items-center">
-                                            <div className="price"><p className="fw-bold fs-4">$120</p></div>
-                                            <div className="buy-btn"><Button variation="success" className="buy-btn">Enroll Now</Button></div>
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <Col>
-                                <Card>
-                                    <Card.Img variant="top" src={php} />
-                                    <Card.Body>
-                                        <p className="fw-bold" style={{ color: '#A3A2A4' }}>Programming</p>
-                                        <Card.Title style={{ fontWeight: 'bold' }}>Learn PHP Programming From Scratch</Card.Title>
-                                        <Card.Text>
-                                            This is a longer card with supporting text below as a natural
-                                            lead-in to additional content. This content is a little bit longer.
-                                        </Card.Text>
-                                        <div className="price-review d-flex justify-content-between align-items-center">
-                                            <div className="price"><p className="fw-bold fs-4">$120</p></div>
-                                            <div className="buy-btn"><Button variation="success" className="buy-btn">Enroll Now</Button></div>
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <Col>
-                                <Card>
-                                    <Card.Img variant="top" src={php} />
-                                    <Card.Body>
-                                        <p className="fw-bold" style={{ color: '#A3A2A4' }}>Programming</p>
-                                        <Card.Title style={{ fontWeight: 'bold' }}>Learn PHP Programming From Scratch</Card.Title>
-                                        <Card.Text>
-                                            This is a longer card with supporting text below as a natural
-                                            lead-in to additional content. This content is a little bit longer.
-                                        </Card.Text>
-                                        <div className="price-review d-flex justify-content-between align-items-center">
-                                            <div className="price"><p className="fw-bold fs-4">$120</p></div>
-                                            <div className="buy-btn"><Button variation="success" className="buy-btn">Enroll Now</Button></div>
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </SwiperSlide>
+                <div>
+                    <Swiper slidesPerView={3} spaceBetween={30} freeMode={true} pagination={{
+                        "clickable": true
+                    }} breakpoints={{
+                        "375": {
+                            "slidesPerView": 1,
+                            "spaceBetween": 20,
+                        },
+                        "425": {
+                            "slidesPerView": 1,
+                            "spaceBetween": 20,
+                        },
+                        "640": {
+                            "slidesPerView": 1,
+                            "spaceBetween": 20
+                        },
+                        "768": {
+                            "slidesPerView": 2,
+                            "spaceBetween": 40
+                        },
+                        "1024": {
+                            "slidesPerView": 3,
+                            "spaceBetween": 50
+                        }
+                    }} autoplay={{
+                        "delay": 2500,
+                        "disableOnInteraction": false
+                    }} className="mySwiper">
+                        <Row xs={1} md={2} className="g-4">
+                            <SwiperSlide>
+                                <Col>
+                                    <Card>
+                                        <Card.Img variant="top" src={php} />
+                                        <Card.Body>
+                                            <p className="fw-bold" style={{ color: '#A3A2A4' }}>Programming</p>
+                                            <Card.Title style={{ fontWeight: 'bold' }}>Learn PHP Programming From Scratch</Card.Title>
+                                            <Card.Text>
+                                                This is a longer card with supporting text below as a natural
+                                                lead-in to additional content. This content is a little bit longer.
+                                            </Card.Text>
+                                            <div className="price-review d-flex justify-content-between align-items-center">
+                                                <div className="price"><p className="fw-bold fs-4">$120</p></div>
+                                                <div className="buy-btn"><Button variation="success" className="buy-btn">Enroll Now</Button></div>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <Col>
+                                    <Card>
+                                        <Card.Img variant="top" src={php} />
+                                        <Card.Body>
+                                            <p className="fw-bold" style={{ color: '#A3A2A4' }}>Programming</p>
+                                            <Card.Title style={{ fontWeight: 'bold' }}>Learn PHP Programming From Scratch</Card.Title>
+                                            <Card.Text>
+                                                This is a longer card with supporting text below as a natural
+                                                lead-in to additional content. This content is a little bit longer.
+                                            </Card.Text>
+                                            <div className="price-review d-flex justify-content-between align-items-center">
+                                                <div className="price"><p className="fw-bold fs-4">$120</p></div>
+                                                <div className="buy-btn"><Button variation="success" className="buy-btn">Enroll Now</Button></div>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <Col>
+                                    <Card>
+                                        <Card.Img variant="top" src={php} />
+                                        <Card.Body>
+                                            <p className="fw-bold" style={{ color: '#A3A2A4' }}>Programming</p>
+                                            <Card.Title style={{ fontWeight: 'bold' }}>Learn PHP Programming From Scratch</Card.Title>
+                                            <Card.Text>
+                                                This is a longer card with supporting text below as a natural
+                                                lead-in to additional content. This content is a little bit longer.
+                                            </Card.Text>
+                                            <div className="price-review d-flex justify-content-between align-items-center">
+                                                <div className="price"><p className="fw-bold fs-4">$120</p></div>
+                                                <div className="buy-btn"><Button variation="success" className="buy-btn">Enroll Now</Button></div>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <Col>
+                                    <Card>
+                                        <Card.Img variant="top" src={php} />
+                                        <Card.Body>
+                                            <p className="fw-bold" style={{ color: '#A3A2A4' }}>Programming</p>
+                                            <Card.Title style={{ fontWeight: 'bold' }}>Learn PHP Programming From Scratch</Card.Title>
+                                            <Card.Text>
+                                                This is a longer card with supporting text below as a natural
+                                                lead-in to additional content. This content is a little bit longer.
+                                            </Card.Text>
+                                            <div className="price-review d-flex justify-content-between align-items-center">
+                                                <div className="price"><p className="fw-bold fs-4">$120</p></div>
+                                                <div className="buy-btn"><Button variation="success" className="buy-btn">Enroll Now</Button></div>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </SwiperSlide>
 
-                    </Row>
-                </Swiper>
+                        </Row>
+                    </Swiper>
+                </div>
             </div>
         </div>
     );
